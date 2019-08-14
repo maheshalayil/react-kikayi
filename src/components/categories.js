4cffd17225f7f449774f2f567ac5b75077f95abf
maheshalayil <dotmahee@gmail.com>
2019-08-14 10:08:09 +0530

    1 file modified

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { useSelector } from 'react-redux'
 
 export const Categories = () => {
+  const categories = useSelector(state => state.categoryState.categories);
+  console.log('categories', categories)
   return (
-    <div>Categories</div>
+    <div>Categories ( {categories.length} )</div>
   )
 }
