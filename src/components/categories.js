37e7a6292746effda5617264ab89a881f1e3d815
maheshalayil <dotmahee@gmail.com>
2019-08-14 10:55:54 +0530

    1 file modified

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux'
 import { loadCategory } from '../actions/category';
 
 export const Categories = () => {
+  
   const dispatch = useDispatch();
+  
   const props = useSelector(state => {
     return {
       categories: state.categoryState.categories
@@ -12,8 +14,21 @@ export const Categories = () => {
   });
 
   console.log('categories', props.categories);
-  //dispatch(loadCategory());
+
+  // useEffect(()=> {
+  //   (async () => {      
+  //     await dispatch(loadCategory())
+  //   })();
+  // }, []);
+
+  const handleClick = async () => {
+    await dispatch(loadCategory());
+  }
+
   return (
-    <div>Categories ( {props.categories.length} )</div>
+    <div>
+      Categories ( {props.categories.length} )
+      <button onClick={handleClick}>load</button>
+    </div>
   )
 }
